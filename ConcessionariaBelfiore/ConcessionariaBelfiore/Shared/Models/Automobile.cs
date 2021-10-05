6a27581053d5316d88775e11a7a6f7c9db44d911
FabiodAgostino <83577536+FabiodAgostino@users.noreply.github.com>
2021-10-05 14:51:39 +0200

    Settings vario

---
+++ ConcessionariaBelfiore/ConcessionariaBelfiore/Shared/Models/Automobile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace ConcessionariaBelfiore.Shared.Models
+{
+    [Table("Automobile")]
+    public partial class Automobile
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Modello { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Marca { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Tipo { get; set; }
+    }
+}
